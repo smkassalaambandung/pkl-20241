9324b2ce4d9380b3c2e115a012fabf0fb6efc851
Agung Wahyudi <olipiskandar@gmail.com>
2024-06-21 15:44:47 +0700

    templating done

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts.backend');
 });
-
+Route::get('data', function () {
+    return view('data');
+});
 Auth::routes(
     ['register' => false]
 );
