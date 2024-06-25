67f5a0d40973e04f5d128102a2bf410b7d4c216d
Agung Wahyudi <olipiskandar@gmail.com>
2024-06-25 13:35:31 +0700

    xxx

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,26 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
+Route::group(['prefix' => 'dashboard', 'middleware' => 'auth'], function(){
+    Route::get('data1000', function () {
+        return view('data');
+    })->name('data');
+    Route::get('data200', function () {
+        return view('data');
+    })->name('data2');
+    Route::get('old', function () {
+        return view('data');
+    })->name('old');
+});
+
 Route::get('/', function () {
     return view('layouts.backend');
-});
+})->middleware('auth');
+
 Route::get('data', function () {
-    return view('data');
+    return view('data')->name('data');
 });
+
 Auth::routes(
     ['register' => false]
 );
